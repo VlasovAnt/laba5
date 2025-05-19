fb8e91b581e1d95bfb29de574e32e6b6863da582
Anton <vlasov.toxa@inbox.ru>
2025-05-19 17:06:01 +0300

    Update Main 4

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print(" добавлено в ветке main");
         System.out.print("first change. very good");
-        System.out.print("third change. why me");
+        System.out.print("fourth change. execellent");
     }
 } 
