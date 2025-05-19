bb2f0c9b899034554cdc199f74d9c0bea5a4206d
Anton <vlasov.toxa@inbox.ru>
2025-05-19 17:03:41 +0300

    Update Main 2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print(" добавлено в ветке main");
         System.out.print("first change. very good");
+        System.out.print("second change. nice");
     }
 } 
