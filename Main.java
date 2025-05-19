6c89fa009f10d0cf62432f226bad12323834e3ff
Anton <vlasov.toxa@inbox.ru>
2025-05-19 17:26:34 +0300

    Update Main third in branch main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
-        System.out.println("new update in Main");
+        System.out.println(" update Main number third in main branch");
     }
 }
